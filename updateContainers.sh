041056e273c9b3364cbb5eafc9eada11c8de79a6
Christian Langner <Cub0n@users.noreply.github.com>
2022-11-08 15:48:44 +0100

    First try

---
+++ updateContainers.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+checkImage() {
+  IMAGE_NAME=$(podman inspect --format '{{ .ImageName }}' "${1}")
+  #echo "Call image ${IMAGE_NAME} with ${1}"
+
+  if [ $( echo "${IMAGE_NAME}" | grep localhost | wc -l ) -eq 0 ]; then
+    podman pull "${IMAGE_NAME}"
+  fi
+
+  IMAGE_DIGEST=$(podman image inspect --format='{{ .Id }}' "${IMAGE_NAME}" | tr -d '"')
+  INSTANCE_DIGEST=$(podman inspect --format='{{ .Image }}' "${1}" | tr -d '"')
+  test "${IMAGE_DIGEST}" = "${INSTANCE_DIGEST}"
+
+  return $?
+}
+
+for pod in `podman ps --format='{{ .Names }}'`
+do
+   checkImage "${pod}"
+   if [ "$?" -ne "0" ]; then
+     echo "Update ${pod}"
+     CMD=$(podman inspect --format '{{ .Config.CreateCommand }}' "${pod}" | tr -d '[]')
+     #echo "${CMD}"
+     "${CMD}"
+   fi
+done
+
+# Cleaning up
+echo "Pruning images, volumes, nets"
+podman system prune -a -f --volumes
